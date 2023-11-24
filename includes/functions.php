fe21f00c8cf0bc1e406936f69ac372e07588e1dc
Hrubák Axel <masodlagos1590@gmail.com>
2023-11-24 14:23:51 +0100

    first_commit

---
+++ includes/functions.php
@@ -0,0 +1,189 @@
+<?php
+
+/* configuration values */
+
+define('APP_BASE', '/PROG/GIT/cikkphp/');
+
+function test($data)
+{
+  print '<pre>';
+  print_r($data);
+  print '</pre>';
+}
+
+function connection()
+{
+  return mysqli_connect('localhost', 'root', '', 'cikk');
+}
+
+function getMenu()
+{
+  return '<div class="nav_bar">
+    <nav>
+        <a href="' . APP_BASE . '' . "kezdolap" . '" class="item">Kezdőlap</a>
+        <a href="' . APP_BASE . '' . "kulfold" . '" class="item">Külföld</a>
+        <a href="#">
+            <h2 id="main_text">Morning News</h2>
+        </a>
+        <a href="' . APP_BASE . '' . "gazdasag" . '" class="item">Gazdaság</a>
+        <a href="' . APP_BASE . '' . "tudomany" . '" class="item">Tudomány</a>
+    </nav>
+</div>';
+}
+
+function parseUrl($url)
+{
+  return str_replace(APP_BASE, '/', $url);
+}
+
+function homepage($connection)
+{
+  $out = '<div class="container">
+  <div class="content">';
+
+  $sql = mysqli_query($connection,  "select * from content");
+
+  while ($cikk = mysqli_fetch_assoc($sql)) {
+
+    $out .= '<div class="articles" id="cikk_' . $cikk["id"] . '">
+          <h4 class="category">' . $cikk["category"] . '</h4>
+          <a href="#cikk_' . $cikk["id"] . '"><h2 class="title">' . $cikk["title"] . '</h2></a>
+          <h4 class="author">Szerző: ' . $cikk["author"] . '</h4>
+          <div class="preview_div"><h3 class="preview">' . $cikk["preview"] . '</h3></div>
+          <div class="content_container">
+              <p>' . $cikk["content_text"] . '</p>
+              <a class="close" href="#">Bezárás</a>
+          </div>
+      </div>';
+  }
+
+  $out .= '</div>
+</div>';
+
+  return $out;
+}
+
+
+function kulfold($connection)
+{
+  $out = '<div class="container">
+  <div class="content">';
+
+  $sql = mysqli_query($connection,  "select * from content");
+
+  while ($cikk = mysqli_fetch_assoc($sql)) {
+
+    if (isset($cikk["category"]) && $cikk["category"] == "Külföld") {
+
+      $out .= '<div class="articles" id="cikk_' . $cikk["id"] . '">
+      <h4 class="category">' . $cikk["category"] . '</h4>
+      <a href="#cikk_' . $cikk["id"] . '"><h2 class="title">' . $cikk["title"] . '</h2></a>
+      <h4 class="author">Szerző: ' . $cikk["author"] . '</h4>
+      <div class="preview_div"><h3 class="preview">' . $cikk["preview"] . '</h3></div>
+      <div class="content_container">
+          <p>' . $cikk["content_text"] . '</p>
+          <a class="close" href="#">Bezárás</a>
+      </div>
+  </div>';
+    }
+  }
+
+  $out .= '</div>
+</div>';
+
+  return $out;
+}
+
+function gazdasag($connection)
+{
+  $out = '<div class="container">
+  <div class="content">';
+
+  $sql = mysqli_query($connection,  "select * from content");
+
+  while ($cikk = mysqli_fetch_assoc($sql)) {
+
+    if (isset($cikk["category"]) && $cikk["category"] == "Gazdaság") {
+
+      $out .= '<div class="articles" id="cikk_' . $cikk["id"] . '">
+      <h4 class="category">' . $cikk["category"] . '</h4>
+      <a href="#cikk_' . $cikk["id"] . '"><h2 class="title">' . $cikk["title"] . '</h2></a>
+      <h4 class="author">Szerző: ' . $cikk["author"] . '</h4>
+      <div class="preview_div"><h3 class="preview">' . $cikk["preview"] . '</h3></div>
+      <div class="content_container">
+          <p>' . $cikk["content_text"] . '</p>
+          <a class="close" href="#">Bezárás</a>
+      </div>
+  </div>';
+    }
+  }
+
+  $out .= '</div>
+</div>';
+
+  return $out;
+}
+
+function tudomany($connection)
+{
+  $out = '<div class="container">
+  <div class="content">';
+
+  $sql = mysqli_query($connection,  "select * from content");
+
+  while ($cikk = mysqli_fetch_assoc($sql)) {
+
+    if (isset($cikk["category"]) && $cikk["category"] == "Tudomány") {
+
+      $out .= '<div class="articles" id="cikk_' . $cikk["id"] . '">
+      <h4 class="category">' . $cikk["category"] . '</h4>
+      <a href="#cikk_' . $cikk["id"] . '"><h2 class="title">' . $cikk["title"] . '</h2></a>
+      <h4 class="author">Szerző: ' . $cikk["author"] . '</h4>
+      <div class="preview_div"><h3 class="preview">' . $cikk["preview"] . '</h3></div>
+      <div class="content_container">
+          <p>' . $cikk["content_text"] . '</p>
+          <a class="close" href="#">Bezárás</a>
+      </div>
+  </div>';
+    }
+  }
+
+  $out .= '</div>
+</div>';
+
+  return $out;
+}
+
+function getContent($url, $connection)
+{
+
+  $url = parseUrl($url);
+
+  $out = '<div class="container border rounded shadow mt-5">';
+
+  switch ($url) {
+    case '/kezdolap':
+      $out = homepage($connection);
+      break;
+
+    case '/kulfold':
+      $out .= kulfold($connection);
+      break;
+
+    case '/gazdasag':
+      $out .= gazdasag($connection);
+      break;
+
+    case '/tudomany':
+      $out .= tudomany($connection);
+      break;
+  }
+  $out .= '</div>';
+
+  return $out;
+}
+
+function getFooter()
+{
+  return '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>';
+}
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include(__DIR__ . "/includes/functions.php");
+
+$connection = connection();
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz@1,6..96&family=Cinzel+Decorative&family=MedievalSharp&family=MonteCarlo&family=Playfair+Display&family=Poppins:wght@100&display=swap');
+    </style>
+    <link rel="stylesheet" href="../cikkphp/styles/user_page.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php print getMenu() ?>
+
+    <?php print getContent($_SERVER["REQUEST_URI"], $connection) ?>
+
+    <?php print getFooter() ?>
+</body>
+
+</html>
